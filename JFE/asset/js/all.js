7204841674cb69f56ad60153d5aa4cecf9f245ab
Liang <92705187+liangfromtaiwan@users.noreply.github.com>
2023-06-08 12:54:31 +0900

    0608 12:54
    0608 12:54

---
+++ JFE/asset/js/all.js
@@ -0,0 +1,32 @@
+/**
+ * @Author: Your name
+ * @Date:   2023-06-05 17:10:08
+ * @Last Modified by:   Your name
+ * @Last Modified time: 2023-06-06 13:45:00
+ */
+
+$(document).ready(function () {
+  // hamburger
+  $(".hamburger").click(function (e) {
+    e.preventDefault();
+    $(".hamburger").toggleClass("active");
+    $(".header__inner").toggleClass("active");
+  });
+
+  //header
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() > 60) {
+      $("#js-nav").addClass("bgWhite");
+    } else {
+      $("#js-nav").removeClass("bgWhite");
+    }
+  });
+
+  // top
+  $(".top").click(function (e) {
+    e.preventDefault();
+    $("html").animate({ scrollTop: 0 }, 500);
+  });
+});
+
+$(function () {});
